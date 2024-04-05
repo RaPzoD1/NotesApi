45347f1229b46b616301ad71299a47b398b027be
Rapzod1 elojme <elojme@gmail.com>
2024-04-05 13:07:12 -0500

    Frontend Added

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const requestLogger = (request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-
+app.use(express.static('dist'))
 const unknownEndpoint = (req,res) =>{
   res.status(404).send({error: 'unknown endpoint'})
 }
